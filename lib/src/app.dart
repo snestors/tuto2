1e1ff55b5f0b0a797bdcecca56ec536974f4a6ad
Nestor <snestors@gmail.com>
2022-03-17 15:59:15 -0500

    Cambios

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Center(child: Text("Hola Mundo.")),
+    );
+  }
+}

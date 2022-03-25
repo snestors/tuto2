b798f6c5dc8ff01b4327e9ce9e56c17cf9fc8145
Nestor <snestors@gmail.com>
2022-03-25 09:59:09 -0500

    25/03/2022 10:00

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tuto/firebase_options.dart';
 import 'src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,10 +4,11 @@ import 'package:tuto/src/pages/alert_page.dart';
 import 'package:tuto/src/pages/avatar_page.dart';
 import 'package:tuto/src/pages/card_page.dart';
 import 'package:tuto/src/pages/home.dart';
+import 'package:tuto/src/pages/user_firebase.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/': (BuildContext context) => const HomePage(),
+    '/': (BuildContext context) => AddUser("Nestor", "company", 20),
     'alert': (BuildContext context) => const AlertPage(),
     'avatar': (BuildContext context) => const AvatarPage(),
     'card': (BuildContext context) => const CardPage(),
